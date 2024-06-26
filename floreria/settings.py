a04fabf33fc0dc750eba160af6743422210d28da
Jordan Thomas Jesús Barra Durán <132635751+jordanbarraduran@users.noreply.github.com>
2024-06-26 00:49:22 -0400

    Añadido inicio y formato de la tienda en templates

--- floreria/settings.py
+++ floreria/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'tienda',
 ]
 
 ROOT_URLCONF = 'floreria.urls'
@@ -121,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- tienda/views.py
+++ tienda/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+    context = {}
+    return render(request, 'tienda/inicio.html', context)
