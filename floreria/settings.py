4a6543e2dea3a6c3aca2ae0e33b22e8d91ceb943
Jordan Thomas Jesús Barra Durán <132635751+jordanbarraduran@users.noreply.github.com>
2024-06-26 00:09:17 -0400

    Añadido app en settings.py

--- floreria/settings.py
+++ floreria/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'tienda',
 ]
 
 ROOT_URLCONF = 'floreria.urls'
