73ecae93d60fc85e4a9b8b16c363b853730b3152
Jordan Thomas Jesús Barra Durán <132635751+jordanbarraduran@users.noreply.github.com>
2024-06-26 22:11:10 -0400

    Añadida vista CRUD en views.py

--- tienda/views.py
+++ tienda/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from .models import Producto, Categoria
+
 # Create your views here.
 def inicio(request):
     context = {}
     return render(request, 'tienda/inicio.html', context)
+
+def crud(request):
+    productos = Producto.objects.all()
+    context = {'productos': productos}
+    return render(request, 'tienda/productos_list.html', context)
