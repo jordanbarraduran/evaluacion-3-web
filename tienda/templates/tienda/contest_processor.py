bb372f4e301f0be6ce78021ad5428156f07d21fd
Jordan Thomas Jesús Barra Durán <132635751+jordanbarraduran@users.noreply.github.com>
2024-06-29 13:34:27 -0400

    Reinicio repositorio

--- tienda/templates/tienda/contest_processor.py
+++
@@ -1,7 +0,0 @@
-def total_carrito(request):
-    total = 0
-    if request.user.is_authenticated:
-        if request.session["carrito"]:
-            for key, value in request.session["carrito"].items():
-                total += int(value["precioacum"])
-    return {"total_carrito" : total}
